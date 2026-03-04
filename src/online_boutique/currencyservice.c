2f207792fa2310fab571ac729c5e08fac264768c
sigshared <researcher2026sigshared@gmail.com>
2026-03-04 14:04:12 -0400

    Warnings fixed

--- src/online_boutique/currencyservice.c
+++ src/online_boutique/currencyservice.c
@@ -406,7 +406,7 @@ static int nf(uint8_t nf_id)
 
     matriz[1][1] = pid_front;
     sigval_t data_send;
-    data_send.sival_ptr = (void *)nf_id;
+    data_send.sival_ptr = (void *)(uintptr_t)nf_id;
     if(sigqueue(pid_front, SIGRTMIN+2, data_send) < 0){
         perror("Error to send signal to gateway");
     }
@@ -432,7 +432,7 @@ static int nf(uint8_t nf_id)
         }
     }
 
-    // IO_RX
+    
     ret = pthread_create(&thread_rx, NULL, &nf_rx, NULL);
     if (unlikely(ret != 0))
     {
@@ -440,7 +440,7 @@ static int nf(uint8_t nf_id)
         return -1;
     }
 
-    // IO_TX
+    
     ret = pthread_create(&thread_tx, NULL, &nf_tx, NULL);
     if (unlikely(ret != 0))
     {
@@ -448,7 +448,7 @@ static int nf(uint8_t nf_id)
         return -1;
     }
 
-    // Cria Workers
+    
     for (i = 0; i < sigshared_cfg->nf[fn_id - 1].n_threads; i++)
     {
         ret = pthread_create(&thread_worker[i], NULL, &nf_worker, (void *)(uint64_t)i);
@@ -459,7 +459,7 @@ static int nf(uint8_t nf_id)
         }
     }
 
-    // Espera as threads teminarem
+    
     for (i = 0; i < sigshared_cfg->nf[fn_id - 1].n_threads; i++)
     {
         ret = pthread_join(thread_worker[i], NULL);
